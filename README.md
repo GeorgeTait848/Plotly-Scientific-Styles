86dbc6166a18f58a930c0eb46d7e27d6f73e3c01
George Tait <george.tait512@gmail.com>
2022-11-01 14:32:52 +0000

    Initial Commit
    Added file containing styles and added README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+# Usage
+
+Simply add the file to the repository you wish to use it in and import the necessary 
+dictionary into the file you are using to make the plot. 
---
+++ styles.py
@@ -0,0 +1,38 @@
+
+scientificPlots = {
+
+    'layout': {
+            'title_x': 0.5,
+            'font': {'size': 15, 'family': 'Calibri', 'color': 'black'},
+            'plot_bgcolor': 'white',
+            
+        },
+
+    'axes': {
+        
+        'xaxis': { 
+            'showline':True,
+            'linewidth':3,
+            'linecolor':'black',
+            'mirror':True,
+            'zeroline': True,
+            'zerolinewidth': 1,
+            'zerolinecolor': 'grey',
+            'gridcolor': 'grey'},
+
+        'yaxis': {
+            'showline':True,
+            'linewidth':3,
+            'linecolor':'black',
+            'mirror':True,
+            'zeroline': True,
+            'zerolinewidth': 1,
+            'zerolinecolor': 'grey',
+            'gridcolor': 'grey'},
+        
+
+    },
+
+    
+
+}
